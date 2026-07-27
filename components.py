918c1e7fb31c00284074b85bfba9182da6ef2d31
teallzy09-byte <teallzy09@gmail.com>
2026-07-27 14:44:20 -0400

    Update components.py

--- components.py
+++ components.py
@@ -124,8 +124,7 @@ def movie_details_dialog(m_id):
             save_table("Reviews", review_db)
             
         st.success("Group settings synchronized!")
-        
-    st.rerun()
+        st.rerun()
 
     st.markdown("---")
     st.markdown("⚠️ **Danger Zone**")
@@ -139,8 +138,7 @@ def movie_details_dialog(m_id):
         save_table("Reviews", updated_reviews)
         
         st.toast("Movie removed successfully!")
-        
-    st.rerun()
+        st.rerun()
 
 @st.dialog("Write or Edit Your Review")
 def edit_review_dialog(m_id, current_user, movie_title):
