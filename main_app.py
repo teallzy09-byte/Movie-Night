c44f00797d8a68e1c91a3d2817315669e16d6a7b
teallzy09-byte <teallzy09@gmail.com>
2026-07-27 11:41:46 -0400

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -6,7 +6,6 @@ from streamlit_gsheets import GSheetsConnection
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 df = conn.read(
-    spreadsheet= "https://docs.google.com/spreadsheets/d/1P1ntjpaQC5pPX_3j-m3GScgJRGqhHalNpumwmOVpeXc/edit",
     worksheet="Sheet1",
     ttl="10m",
     usecols=[0, 1],
